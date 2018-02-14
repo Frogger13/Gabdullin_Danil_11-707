da2d8127bfcc486beeb375eb89a81029223829ed
Gabdullin_Danil_11-707 <gfg071298@yandex.ru>
2018-02-14 11:07:21 +0300

    Added Ps1 Part4 Ex12

---
+++ SEM/Ps1Part4Ex12.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int m = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+            int x, y, z;
+            int n = 0;
+            for (x=0;x<m;x++)
+                for (y=0;y<=m;y++)
+                    for (z=0;z<m;z++)
+                    {
+                        if ((Math.Pow(x, k) % m) == (Math.Pow(y, k) % m) + (Math.Pow(z, k) % m))
+                            n++;
+                    }
+            Console.WriteLine(n);
+        }
+    }
+}
